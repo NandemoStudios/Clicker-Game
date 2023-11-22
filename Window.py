4643c59242ba6597c3c631dbfe8c908f5e1f7045
NandemoStudios <NandemoStudios@users.noreply.github.com>
2023-11-22 00:38:35 +0000

    - changed the exit buttons command to work properly

--- Window.py
+++ Window.py
@@ -57,7 +57,7 @@ class Window():
         self.game_menu.add_command(label="Save", command=self.Save, accelerator="Ctrl+S")
         self.game_menu.add_command(label="Load", command=self.Load, accelerator="Crtl+L")
         self.game_menu.add_separator()
-        self.game_menu.add_command(label="Exit", command=quit)
+        self.game_menu.add_command(label="Exit", command=self.root.quit)
         self.toolbar.add_cascade(label="Game", menu=self.game_menu)
         self.root.config(menu=self.toolbar)
         
